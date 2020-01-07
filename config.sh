78da89afb813936c77f1fc8fbe9ab622794d1719
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2020-01-07 21:05:13 +0530

    Add config file

---
+++ config.sh
@@ -0,0 +1,28 @@
+# none, aws, or openstack
+export PLATFORM=none
+
+# network type
+export NETWORK_TYPE=OpenShiftSDN
+
+# the base domain for the cluster
+export BASE_DOMAIN=nip.io
+
+# the name external name on which to connect to the user cluster API server
+# IMPACT: kube-apiserver cert generation and admin.kubeconfig server URL
+export EXTERNAL_API_DNS_NAME=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1').${BASE_DOMAIN}
+
+# the external IP on which to connect to the user cluster API server
+export EXTERNAL_API_IP_ADDRESS=$(dig +short ${EXTERNAL_API_DNS_NAME})
+
+# the external port on which to connect to the user cluster API server
+# IMPACT: admin.kubeconfig server URL
+export EXTERNAL_API_PORT=6443
+
+# the cluster IP to use for openshift-apiserver
+export API_CLUSTERIP=172.30.0.20
+
+# OKD/OCP release image from which to get component image pull specs
+export RELEASE_IMAGE="quay.io/openshift-release-dev/ocp-release:4.2.13"
+
+# the subdomain to be used for the ingress router
+export INGRESS_SUBDOMAIN="apps-${EXTERNAL_API_DNS_NAME}"
